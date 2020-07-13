bfcb60ef90b806c6c842ae5979ce88a7a72188f5
callmexss <callmexss@126.com>
2020-07-13 17:42:27 +0800

    save answer for E2-04

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,15 +2,19 @@
 #
 # Exercise 1.27
 
+import sys
+import csv
+
 def portfolio_cost(filename):
     cost = 0
     with open(filename) as f:
+        rows = csv.reader(f)
         start = True
-        for line in f:
+        for row in rows:
             if start:
                 start = False
                 continue
-            _, share, price = line.split(",")
+            _, share, price = row
             try:
                 cost += int(share) * float(price)
             except ValueError:
@@ -24,3 +28,9 @@ cost = portfolio_cost("Data/portfolio.csv")
 print(f"Total cost {cost}")
 
 cost = portfolio_cost("Data/missing.csv")
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    cost = portfolio_cost(filename)
+    print(f"Total cost {cost}")
+
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,21 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+
+def read_portfolio(filename):
+    portfolio_list = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            portfolio_list.append((row[0], int(row[1]), float(row[2])))
+    return portfolio_list
+
+
+li = read_portfolio("Data/portfolio.csv")
+print(li)
+
+
+
